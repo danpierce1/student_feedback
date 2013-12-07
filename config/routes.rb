f3f599f6808193fb10d107c784846e37c964dc1f
Dan Pierce <20045260@mail.wit.ie>
2013-12-07 23:24:09 +0000

    Finsh sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 StudentFeedback::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
-  root to: 'static_pages#home'
+  root  to:'static_pages#home'
+  
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/home',    to: 'static_pages#home',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
