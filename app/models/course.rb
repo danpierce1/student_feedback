a65fd90a3d101a675592bcdf607410988d12007e
Dan Pierce <20045260@mail.wit.ie>
2013-12-09 03:49:27 +0000

    finished!!

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
   attr_accessible :name
-   has_many :users
+   # has_many :users
    validates :name, presence: true
-     has_and_belongs_to_many :cmodules  # NEW
+     # has_and_belongs_to_many :cmodules  # NEW
 
 end
