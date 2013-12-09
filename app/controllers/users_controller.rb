1a05e22dae080fbb989983ed8734b60b7a126bb6
Dan Pierce <20045260@mail.wit.ie>
2013-12-09 03:17:22 +0000

    styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Student Feedback!"
+      flash[:success] = "Welcome to Student Feedback!"
       redirect_to @user
     else
       render 'new'
